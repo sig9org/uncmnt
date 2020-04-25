626c9c3b46a3593fb60b7395f74e0d4f573a2643
sig9org <sig9@sig9.org>
2020-04-26 01:30:07 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# uncmnt
+
+Display the contents of the file, excluding comment lines and blank lines.
---
+++ uncmnt.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	r1 := regexp.MustCompile(`^\s*#`)
+	r2 := regexp.MustCompile(`^\s*$`)
+	flag.Parse()
+	args := flag.Args()
+	for _, v := range args{
+		fp, err := os.Open(v)
+		if err != nil{
+			//fmt.Println("error")
+		}
+		defer fp.Close()
+
+		scanner := bufio.NewScanner(fp)
+		for scanner.Scan() {
+			txt := scanner.Text()
+			ms1 := r1.MatchString(txt)
+			ms2 := r2.MatchString(txt)
+			if !ms1 && !ms2 {
+				fmt.Println(scanner.Text())
+			}
+		}
+	}
+}
